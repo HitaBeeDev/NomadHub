b041090d4734b5470f4518f99d01a1eb18eb6ef3
HitaBeeDev <anahita.amiri@gmail.com>
2024-09-16 21:33:19 +0300

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,14 @@
 import "./index.css";
+import {Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <header className="p-5 flex flex-row justify-between items-center">
-        <p className="font-medium text-xl">NomadHub</p>
+<Routes>
+  <Route index element={}
+</Routes>
 
-        <p>ANYWHERE</p>
 
-        <p>WEEK</p>
-
-        <p>GUESTS</p>
-
-        <p>SEARCH</p>
-      </header>
-    </div>
+   
   );
 }
 
---
+++ client/src/Pages/IndexPage.jsx
@@ -0,0 +1,19 @@
+function IndexPage() {
+  return (
+    <div>
+      <header className="p-5 flex flex-row justify-between items-center">
+        <p className="font-medium text-xl">NomadHub</p>
+
+        <p>ANYWHERE</p>
+
+        <p>WEEK</p>
+
+        <p>GUESTS</p>
+
+        <p>SEARCH</p>
+      </header>
+    </div>
+  );
+}
+
+export default IndexPage;
