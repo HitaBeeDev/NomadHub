3d76e5d3cfb3444e695e7a7a48666e68fddd2667
HitaBeeDev <anahita.amiri@gmail.com>
2024-09-23 21:14:57 +0300

    commit

--- client/src/Pages/LoginPage.jsx
+++ client/src/Pages/LoginPage.jsx
@@ -7,28 +7,31 @@ function LoginPage() {
         <BackgroundGif />
       </div>
 
-      <div className="col-span-1">
-        <p>Welcome back!</p>
+      <div className="col-span-1 flex flex-col p-10 mx-auto">
+        <p className="text-3xl font-semibold font-mono leading-normal text-left">
+          Welcome back!
+        </p>
+        <p className="text-sm font-normal text-left">
+          Please enter your details.
+        </p>
 
-        <p>Please enter your details.</p>
+        <div className="flex flex-col mt-8 gap-1 text-left">
+          <label className="text-sm font-normal">Email</label>
 
-        <div>
-          <label>Email</label>
-
-          <input type="email" />
+          <input type="email" className="rounded-md h-8" />
         </div>
 
-        <div>
-          <label>Password</label>
+        <div className="flex flex-col gap-1 mt-4 text-left">
+          <label className="text-sm font-normal">Password</label>
 
-          <input type="password" />
+          <input type="password" className="rounded-md h-8" />
         </div>
 
-        <button>Sign in</button>
+        <button className="mt-4">Sign in</button>
 
-        <div>
-          <p>Don't have an account? </p>
-          <Link to={"/register"} className="bg-red-200 mt-5">
+        <div className="mt-4 text-left">
+          <p>Don't have an account?</p>
+          <Link to={"/register"} className="bg-red-200 mt-1">
             Register Here
           </Link>
         </div>
